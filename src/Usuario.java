07ae1402f90f6d58fd9d030aa9cec7f68b9dad4a
Mateus <mateusvidolin@outllok.com>
2023-03-04 07:54:44 -0300

    Desenvolvido algoritimo para controlar a smart tv

---
+++ src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+        smartTv.mudarCanal(15);
+        System.out.println("Canal Atual: = " + smartTv.canal);
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> Tv ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> Tv ligada? " + smartTv.ligada);
+    }
+}
